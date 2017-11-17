368283663efda6f18b8a34202b4d33c9690a8864
jhaudry <jeremy-haudry@hotmail.fr>
2017-11-17 08:07:58 +0100

    ingore some files

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# boot-dev
-
-Just a fast booter of an PHP-Nginx-Mariadb development environment with docker
+PhpUnitExchangeExo
--- app/index.php
+++ app/index.php
@@ -1,2 +1,27 @@
 <?php
-phpinfo();
+
+require_once 'Autoloader.php';
+
+use Exo\Autoloader;
+use Exo\User;
+use Exo\Exchange;
+use Exo\Product;
+
+Autoloader::register();
+
+$receiver = new User('toto@tutu.fr', 'toto', 'tutu', 19);
+$owner = new User('titi@tata.fr', 'titi', 'tata', 19);
+$product = new Product('saucisson', 'available', $owner);
+$startDate = '12/11/2017';
+$endDate = '15/11/2017';
+
+$exchange = new Exchange();
+
+//$save = $exchange->save($receiver, $product, $owner, $startDate, $endDate);
+
+$dates = $exchange->areValidDates($startDate, $endDate);
+
+//var_dump($save);
+var_dump($dates);
+
+//$start = DateTime::createFromFormat('m/d/Y', $startDate);
